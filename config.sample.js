1cbb4dd2c0f88588910b0db618216bcd4c8a2666
Dale Higgs <dale3h@gmail.com>
2016-03-31 03:18:19 -0500

    fix formatting, repo, and dependencies

--- config.sample.js
+++
@@ -1,9 +0,0 @@
-+'use strict';
- 
- module.change_code = 1;
- 
- module.exports = {
- 	username: 'mint username',
- 	password: 'mint password',
- 	debug: true,
- };
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-module.change_code = 1;
-
 'use strict';
 
-var alexa = require( 'alexa-app' );
-var config  = require( './config' );
-var levenshtein = require('fast-levenshtein');
-var app = new alexa.app( 'mint-check' );
+module.change_code = 1;
 
+var alexa       = require('alexa-app'),
+	config      = require('./config.json'),
+	levenshtein = require('fast-levenshtein'),
+	app         = new alexa.app('mint-check');
 
 app.launch( function( request, response ) {
 	response.say( 'Welcome to Mint Account Balance Checker.' ).reprompt( 'Say Check Balance of account name to get its balance or say list accounts to hear all options' ).shouldEndSession( false );
@@ -16,7 +15,7 @@ app.error = function( exception, request, response ) {
 	console.log(exception)
 	console.log(request);
 	console.log(response);
-	
+
 	handleError(response,exception);
 };
 
@@ -29,35 +28,35 @@ app.intent( 'listAccountsIntent', {
 		"my accounts",
 		"name accounts",
 	] }, function( request, response ) {
-	
+
 		console.log('List accounts invoked');
-		
+
 		var responseString = 'Your accounts are. ';
-		
+
 		getMintData(config.username,config.password,function(success,error,mintAccounts){
 			console.log('Got Mint Data!');
-			
+
 			if(success == false)
 			{
 				handleError(response,error);
 			}
 			else
-			{		
+			{
 				var index = 1;
-				
+
 				console.log('Iterating mint accounts');
-				
+
 				mintAccounts.forEach(function(account) {
 					responseString += index + '. ' + account.fiLoginDisplayName + ' ' + account.accountName + '. ';
 					index++;
 				});
-				
+
 				console.log('Sending Response ' + responseString);
-				
-				response.say( responseString ).send();	
+
+				response.say( responseString ).send();
 			}
 		});
-		
+
 		return false;
 	}
 );
@@ -69,20 +68,20 @@ app.intent( 'refreshData', {
 		"reload data",
 		"update accounts",
 	] }, function( request, response ) {
-	
+
 		getMintData(config.username,config.password,function(success,error,mintAccounts){
 			if(success == false)
 			{
 				handleError(response,error);
 			}
 			else
-			{		
-				response.say( 'Reloaded data for ' +  mintAccounts.length + ' accounts').send();	
+			{
+				response.say( 'Reloaded data for ' +  mintAccounts.length + ' accounts').send();
 			}
 		});
-		
+
 		return false;
-			
+
 	}
 );
 
@@ -93,25 +92,25 @@ app.intent( 'listAllBalances', {
 		"get all",
 		"read all",
 	] }, function( request, response ) {
-	
+
 		getMintData(config.username,config.password,function(success,error,mintAccounts){
 			if(success == false)
 			{
 				handleError(response,error);
 			}
 			else
-			{		
+			{
 				var responseString = '';
 				mintAccounts.forEach(function(account) {
-					
+
 					responseString += 'The balance of account ' + account.fiLoginDisplayName + ' is ' +  account.value + ' ' + account.currency + '. ';
 				});
 				response.say(responseString).send();
 			}
 		});
-		
+
 		return false;
-			
+
 	}
 );
 
@@ -125,18 +124,18 @@ app.intent( 'getBalanceIntent', {
 		"check balance {!ACCOUNT_NAME|Account}",
 		"{!ACCOUNT_NAME|Account}",
 	] }, function( request, response ) {
-				
+
 		//the name of the account the person wants to get the balance for.
 		var requestedAccount = request.slot('Account');
-		
+
 		console.log('Looking for an account with the name ' + requestedAccount);
-		
+
 		//container to hold data of requested account when/if it is found.
 		var requestedAccountData = new Object();
 
 		getMintData(config.username,config.password,function(success,error,mintAccounts){
 			//iterate over the accounts and attempt to find the one the user wants.
-			
+
 			if(success == false)
 			{
 				handleError(response,error);
@@ -147,26 +146,26 @@ app.intent( 'getBalanceIntent', {
 				var bestMatchScore = 100;
 				var currentIndex = 0;
 				var requestedAccountData;
-				
+
 				mintAccounts.forEach(function(account) {
 					var checkAccount = account.fiName.toLowerCase();
-					
+
 					var thisScore = levenshtein.get(checkAccount, requestedAccount);
-					
+
 					if(thisScore < bestMatchScore)
 					{
 						bestMatchIndex = currentIndex;
 						bestMatchScore = thisScore;
 					}
 				});
-				
+
 				if(bestMatchScore < 5 && bestMatchIndex != -1)
 				{
 					requestedAccountData = mintAccounts[bestMatchIndex];
 				}
-		
+
 				//read off the balance.
-		
+
 				if(bestMatchIndex != -1)
 				{
 					response.say( 'The balance of account ' + requestedAccountData.fiLoginDisplayName + ' is ' +  requestedAccountData.value + ' ' + requestedAccountData.currency + ' last updated ' + requestedAccountData.lastUpdatedInString + ' ago').send();
@@ -177,7 +176,7 @@ app.intent( 'getBalanceIntent', {
 				}
 			}
 		});
-		
+
 		return false;
 	}
 );
@@ -195,7 +194,7 @@ function getMintData(username,password,callback)
 	var mintAccountArray = [];
 	require('pepper-mint')(username, password).then(function(mint) {
 		console.log(username + " logged in...");
-	 
+
 		return mint.getAccounts();
 	}).then(function(accounts) {
 		console.log('Got ' + accounts.length + '  accounts');
@@ -205,17 +204,17 @@ function getMintData(username,password,callback)
 				mintAccountArray.push(account);
 			}
 		});
-		
+
 
 		if(typeof callback == 'function')
 		{
-			callback(true,{},mintAccountArray);		
+			callback(true,{},mintAccountArray);
 		}
 	}).fail(function(err) {
 		callback(false,err,mintAccountArray);
 	})
-	
-	
+
+
 }
 
 function handleError(response,error)
